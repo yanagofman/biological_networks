acf82456aec3fb3505a3e61997de0c3a2c5bad2a
LordNoProblemo <nimrod.pinkus@gmail.com>
2017-10-14 10:56:20 +0300

    Add files via upload

--- model/data_test.py
+++ model/data_test.py
@@ -44,6 +44,48 @@ def build_classifier(traindata_filename):
 
     return rfClf  # , svmClf
 
+def build_classifier_for_celline(traindata_filename,skip = False):
+    data = list()
+    lbls = np.array([])
+
+    with open(traindata_filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        first_line = False
+        j = 0
+        for row in reader:
+            if not first_line:
+                first_line = True
+                continue
+            j+=1
+            if skip and j == 2:
+                continue
+            lbls = np.append(lbls, float(row[1]))
+            vec = np.array(row[2:])
+            vec[vec == ''] = '0'
+            data.append(vec.astype(int))
+
+    print("---finished reading data file---")
+    data = np.array(data)
+    tresh1 = np.average(lbls) + np.sqrt(np.var(lbls))
+    tresh2 = np.average(lbls) - np.sqrt(np.var(lbls))
+    data1 = data[lbls > tresh1]
+    data2 = data[lbls <= tresh2]
+    lbls1 = lbls[lbls > tresh1]
+    lbls2 = lbls[lbls <= tresh2]
+    lbls = np.append(lbls1, lbls2)
+    data = np.append(data1, data2, axis=0)
+
+    lbls[lbls > tresh1] = 1
+    lbls[lbls <= tresh2] = 0
+
+    print("---finished building data set---")
+
+    rfClf = RF(data, lbls, int(0.9 * len(data)))
+    print("---finished building classifiers---")
+    pkl.dump(rfClf, open('random_forest_classifier.p', 'wb'))
+    print("---done---")
+
+    return rfClf 
 
 def build_classifier2(traindata_filename):
     data_dict = dict()
@@ -231,3 +273,55 @@ def test_classifiers(rfClf, svmClf=None):
     plt.legend(bbox_to_anchor=(0, -0.95), loc=2, borderaxespad=0.)
     plt.savefig("ROC curves.png")
     plt.show()
+
+
+def compare_to_random_roc(file_name, builder = build_classifier2 ):
+    original_clf = builder("training_set_files/no_shuffle/"+file_name+".csv")
+    sizes = [int((0.5 + 0.1 * i) * original_clf.size) for i in range(5)]
+    or_fprs,or_tprs,or_auc = original_clf.ROC(sizes,1)
+    rand_roc = dict()
+    rocs = []
+    for i in range(1,11):
+        clf = builder("training_set_files/shuffle_"+str(i)+"/"+file_name+".csv")
+        fprs,tprs,t_auc = clf.ROC(sizes,1)
+        rocs+=[(fprs,tprs,t_auc)]
+        for i in range(len(fprs)):
+            if(fprs[i] in rand_roc):
+                rand_roc[fprs[i]] += [tprs[i],1]
+            else:
+                rand_roc[fprs[i]] = np.array([tprs[i],1])
+    ran_fprs = sorted(list(rand_roc.keys()))
+    ran_tprs = []
+    for i in range(len(ran_fprs)):
+        ran_tprs += [rand_roc[ran_fprs[i]][0] / rand_roc[ran_fprs[i]][1]]
+    ran_auc = auc(ran_fprs,ran_tprs)
+    plt.figure(1)
+    plt.subplot(211)
+    plt.ylim(0, 1)
+    plt.xlim(0, 1)
+    plt.ylabel("True Positive rate")
+    plt.xlabel("False Positive Rate")
+    plt.plot([0, 1], [0, 1], 'r-')
+    legend = []
+    or_roc, = plt.plot(or_fprs, or_tprs, 'b-', label="RF ROC, auc = " + str(or_auc))
+    legend+=[or_roc]
+    ran_roc, = plt.plot(ran_fprs, ran_tprs, 'y-', label="RF ROC of average of shuffles, auc = " + str(ran_auc))
+    legend+=[ran_roc]
+    plt.legend(handles=legend)
+    plt.legend(bbox_to_anchor=(0, -0.95), loc=2, borderaxespad=0.)
+    plt.savefig("graph_results/"+file_name+"/Roc curve.png")
+    plt.show()
+    for i in range(10):
+        plt.subplot(211)
+        plt.ylim(0, 1)
+        plt.xlim(0, 1)
+        plt.ylabel("True Positive rate")
+        plt.xlabel("False Positive Rate")
+        plt.plot([0, 1], [0, 1], 'r-')
+        gr, = plt.plot(rocs[i][0], rocs[i][1], 'b-', label="RF shuffle "+str(i+1)+" ROC, auc = " + str(rocs[i][2]))
+        legend =[gr]
+        plt.legend(handles=[gr])
+        plt.legend(bbox_to_anchor=(0, -0.95), loc=2, borderaxespad=0.)
+        plt.savefig("graph_results/"+file_name+"/Roc curve - shuffle "+str(i+1)+".png")
+        plt.show()
+    
