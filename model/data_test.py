ea771eff15963d827dbb3479a3e08c066ff45f2b
LordNoProblemo <nimrod.pinkus@gmail.com>
2017-10-16 22:49:46 +0300

    Add files via upload

--- model/data_test.py
+++ model/data_test.py
@@ -2,7 +2,7 @@ import csv
 import pickle as pkl
 
 from create_model import *
-
+from sklearn.ensemble import RandomForestRegressor
 
 def build_classifier(traindata_filename):
     data = list()
@@ -43,6 +43,8 @@ def build_classifier(traindata_filename):
     print("---done---")
 
     return rfClf  # , svmClf
+
+
 
 def build_classifier_for_celline(traindata_filename,skip = False):
     data = list()
@@ -86,6 +88,97 @@ def build_classifier_for_celline(traindata_filename,skip = False):
     print("---done---")
 
     return rfClf 
+
+def load_data1():
+    data_dict = dict()
+    lbls_dict = dict()
+
+    with open(traindata_filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        first_line = False
+        j = 0
+        for row in reader:
+            if not (first_line):
+                first_line = True
+                continue
+            if row[0] in data_dict:
+                lbls_dict[row[0]] = np.append(lbls_dict[row[0]], float(row[2]))
+                vec = np.array(row[3:])
+                vec[vec == ''] = '0'
+                data_dict[row[0]].append(vec.astype(int))
+            else:
+                lbls_dict[row[0]] = np.array(float(row[2]))
+                vec = np.array(row[3:])
+                vec[vec == ''] = '0'
+                data_dict[row[0]] = [vec.astype(int)]
+
+    print("---finished reading data file---")
+    data = None
+    lbls = np.array([])
+    for cell in data_dict:
+        print('---', cell, '---')
+        print(len(data_dict[cell]))
+        tresh1 = np.average(lbls_dict[cell]) + np.sqrt(np.var(lbls_dict[cell]))
+        tresh2 = np.average(lbls_dict[cell]) - np.sqrt(np.var(lbls_dict[cell]))
+        data1 = np.array(data_dict[cell])
+        lbls1 = lbls_dict[cell][lbls_dict[cell] > tresh1]
+
+        lbls = np.append(lbls, lbls1)
+        if data != None:
+            data = np.append(data, data1, axis=0)
+        else:
+            data = data1
+
+    print("---finished building data set---")
+    
+    return data,lbls
+
+def load_data(traindata_filename,skip = False):
+    data_dict = dict()
+    lbls_dict = dict()
+
+    with open(traindata_filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        first_line = False
+        j = 0
+        for row in reader:
+            if not (first_line):
+                first_line = True
+                continue
+            if row[0] in data_dict:
+                lbls_dict[row[0]] = np.append(lbls_dict[row[0]], float(row[2]))
+                vec = np.array(row[3:])
+                vec[vec == ''] = '0'
+                data_dict[row[0]].append(vec.astype(int))
+            else:
+                lbls_dict[row[0]] = np.array(float(row[2]))
+                vec = np.array(row[3:])
+                vec[vec == ''] = '0'
+                data_dict[row[0]] = [vec.astype(int)]
+
+    print("---finished reading data file---")
+    data = None
+    lbls = np.array([])
+    for cell in data_dict:
+        print('---', cell, '---')
+        print(len(data_dict[cell]))
+        tresh1 = np.average(lbls_dict[cell]) + np.sqrt(np.var(lbls_dict[cell]))
+        tresh2 = np.average(lbls_dict[cell]) - np.sqrt(np.var(lbls_dict[cell]))
+        data1 = np.array(data_dict[cell])[lbls_dict[cell] > tresh1]
+        data2 = np.array(data_dict[cell])[lbls_dict[cell] <= tresh2]
+        lbls1 = lbls_dict[cell][lbls_dict[cell] > tresh1]
+        
+        lbls2 = lbls_dict[cell][lbls_dict[cell] <= tresh2]
+        lbls = np.append(lbls, lbls1)
+        lbls = np.append(lbls, lbls2)
+        if data != None:
+            data = np.append(data, np.append(data1, data2, axis=0), axis=0)
+        else:
+            data = np.append(data1, data2, axis=0)
+
+    print("---finished building data set---")
+    return data,lbls
+
 
 def build_classifier2(traindata_filename):
     data_dict = dict()
@@ -324,4 +417,53 @@ def compare_to_random_roc(file_name, builder = build_classifier2 ):
         plt.legend(bbox_to_anchor=(0, -0.95), loc=2, borderaxespad=0.)
         plt.savefig("graph_results/"+file_name+"/Roc curve - shuffle "+str(i+1)+".png")
         plt.show()
+        
+def compare_to_random_reg(file_name, builder = load_data ):
+    rf = RandomForestRegressor(n_jobs = -1)
+    dt,lb = builder("training_set_files/no_shuffle/"+file_name+".csv")
+    sizes = [int((0.7 + 0.15 * j) * len(lb)) for j in range(2)]
+    test_acc = []
+    train_acc = []
+    for size in sizes:
+        train_dt = dt[:size]
+        train_lb = lb[:size]
+        test_dt = dt[size+1:]
+        test_lb = lb[size+1:]
+        rf.fit(train_dt,train_lb)
+        test_acc += [np.sum(abs(rf.predict(test_dt) - test_lb) <= 10**-7)/len(test_dt)]
+        train_acc += [np.sum(abs(rf.predict(train_dt) - train_lb) <= 10**-7)/len(train_dt)]
+    rand_test_acc = np.zeros(2)
+    rand_train_acc = np.zeros(2)
+    for i in range(1,11):
+        temp_test_acc = []
+        temp_train_acc = []
+        print("---",i,"---")
+        temp_dt, temp_lb = builder("training_set_files/shuffle_"+str(i)+"/"+file_name+".csv")
+        
+        for size in sizes:
+            train_dt = temp_dt[:size]
+            train_lb = temp_lb[:size]
+            test_dt = temp_dt[size+1:]
+            test_lb = temp_lb[size+1:]
+            rf.fit(train_dt,train_lb)
+            temp_test_acc += [np.sum(abs(rf.predict(test_dt) - test_lb) <= 10**-7)/len(test_dt)]
+            temp_train_acc += [np.sum(abs(rf.predict(train_dt) - train_lb) <= 10**-7)/len(train_dt)]
+        rand_test_acc += temp_test_acc
+        rand_train_acc += temp_train_acc
+    rand_test_acc /= 10
+    rand_train_acc/=10
+    plt.subplot(211)
+    #plt.ylim(0, 1)
+    #plt.xlim(0, 1)
+    plt.ylabel("Accuracy")
+    plt.xlabel("Train data size")
+    test, = plt.plot(sizes, test_acc, 'g-', label="Rf Regression accuracy of real data - test")
+    train, = plt.plot(sizes, train_acc, 'b-', label="Rf Regression accuracy of real data - train")
+    rand_test, = plt.plot(sizes, rand_test_acc, 'r-', label="Rf Regression accuracy of average random data - test")
+    rand_train, = plt.plot(sizes, rand_train_acc, 'y-', label="Rf Regression accuracy of average random data - train")
+    plt.legend(handles=[test,train,rand_test,rand_train])
+    plt.legend(bbox_to_anchor=(0, -0.95), loc=2, borderaxespad=0.)
+    plt.savefig("graph_results/"+file_name+"/Roc curve - shuffle "+str(i+1)+".png")
+    plt.show()
+        
     
